5b9507f475307dd3c3b6ec9c54522beafcca338f
SilvioMonnerat <silvio@cabanacriacao.com>
2013-07-05 23:11:48 -0300

    _inicialize or project

---
+++ wp-content/themes/VidaUtil/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div id="header"></div> <!-- end of #header -->
+
+	<div id="content"></div> <!-- end of #content -->
+
+	<div id="sidebar"></div> <!-- end of #sidebar -->
+
+	<div id="footer"></div> <!-- end of #footer -->
+</body>
+</html>
