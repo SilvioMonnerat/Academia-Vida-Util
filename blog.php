80d76f2cf13b5dcc2ade3db008f9fa5d11761d06
SilvioMonnerat <silvio@cabanacriacao.com>
2016-12-18 22:17:53 -0200

    blog
    fkasdf df,asdkf pdf asdfa fasf

---
+++ blog.php
@@ -0,0 +1,268 @@
+<?php include 'header.php'; ?>
+	<section class="banner" style="background-image: url('library/images/banners/banner-blog.jpg')">
+		<div class="container">
+			<h2 class="banner--title">Blog Huma</h2>
+		</div>
+	</section>
+
+	<section class="container-fluid inner-blog">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<aside class="col-xs-12 col-sm-3 col-md-3">
+						<form action="#" class="blog-search">
+							<input type="search" class="blog-search--input" name="busca" id="busca" placeholder="Buscar">
+						</form>
+						<ul class="blog-menu">
+							<li class="blog-menu--item active">
+								<a href="blog-categoria.php">
+									<i class="blog-menu--icon">
+										<img src="library/images/icons/blog-cidade.png" alt="">
+									</i>
+									<span class="blog-menu--text">Arquitetura e cidade</span>
+								</a>
+							</li>
+							<li class="blog-menu--item">
+								<a href="blog-categoria.php">
+									<i class="blog-menu--icon">
+										<img src="library/images/icons/blog-cidadao.png" alt="">
+									</i>
+									<span class="blog-menu--text">O morador é cidadão</span>
+								</a>
+							</li>
+							<li class="blog-menu--item">
+								<a href="blog-categoria.php">
+									<i class="blog-menu--icon">
+										<img src="library/images/icons/blog-dialogos.png" alt="">
+									</i>
+									<span class="blog-menu--text">Dialogos da Huma</span>
+								</a>
+							</li>
+							<li class="blog-menu--item">
+								<a href="blog-categoria.php">
+									<i class="blog-menu--icon">
+										<img src="library/images/icons/blog-consumo.png" alt="">
+									</i>
+									<span class="blog-menu--text">Consumo consciente</span>
+								</a>
+							</li>
+							<li class="blog-menu--item">
+								<a href="blog-categoria.php">
+									<i class="blog-menu--icon">
+										<img src="library/images/icons/blog-inspiracoes.png" alt="">
+									</i>
+									<span class="blog-menu--text">Inspirações</span>
+								</a>
+							</li>
+						</ul>
+					</aside>
+					<section class="col-xs-12 col-sm-9 col-md-9">
+						<ul class="blog-list row">
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+							<li class="blog-list--item col-xs-12 col-sm-6 col-md-6">
+								<a href="post.php" class="blog-list--link">
+									<div class="blog-list--image" style="background-image: url('library/images/thumbs/blog-1.jpg');">
+										<img src="library/images/thumbs/blog-1.jpg" alt="">
+										<span class="blog-list--category">Arquitetura e cidade</span>
+										<div class="blog-list--cover">
+											<i class="fa fa-plus"></i>
+										</div>
+									</div>
+									<div class="blog-list--content">
+										<h3 class="blog-list--title">Cidades com alma</h3>
+										<p class="blog-list--text">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja implesmente uma simulação de texto</p>
+										<span class="blog-list--date">21 de Setembro de 2016</span>
+									</div>
+								</a>
+							</li>
+						</ul>
+						<div class="blog-pagination">
+							<ul class="blog-pagination--list">
+								<li class="prev">
+									<a href="#">
+										<i class="fa fa-angle-left"></i>
+									</a>
+								</li>
+								<li class="next">
+									<a href="#">
+										<i class="fa fa-angle-right"></i>
+									</a>
+								</li>
+
+								<li class="number">
+									<a href="#">
+										01
+									</a>
+								</li>
+								<li class="number">
+									<a href="#">
+										02
+									</a>
+								</li>
+								<li class="number">
+									<a href="#">
+										03
+									</a>
+								</li>
+								<li class="number">
+									<a href="#">
+										04
+									</a>
+								</li>
+								<li class="number">
+									<a href="#">
+										05
+									</a>
+								</li>
+							</ul>
+						</div>
+					</section>
+				</div>
+			</div>
+		</div>
+	</section>
+<?php include 'footer.php'; ?>
+
